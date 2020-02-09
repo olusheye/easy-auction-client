7c90f1a8baeae8df8d70a6e24874ec600f62c33a
Omoleye Oluseye <sshheye@gmail.com>
2020-02-09 23:47:42 +0100

    fix(cleanup):remove unnecessary folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,15 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AppErrorHandler } from './common/errors/app-error-handler';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-// used to create fake backend
-// import { fakeBackendProvider } from './_helpers/fake-backend';
 import { JwtInterceptor } from './_helpers/jwt.interceptor';
 import { ErrorInterceptor } from './_helpers/error.interceptor';
-import { AlertComponent } from './_components/alert/alert.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [AppComponent, AlertComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -29,8 +25,6 @@ import { environment } from '../environments/environment';
     {
       provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true,
     },
-    // {provide: ErrorHandler, useClass: AppErrorHandler},
-    // fakeBackendProvider
   ],
   bootstrap: [AppComponent],
   exports: []
